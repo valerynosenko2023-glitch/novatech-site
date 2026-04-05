d7b97cf788c5659e9f895289d17f7d1bce04af95
valerynosenko2023-glitch <valery.nosenko2023@icloud.com>
2026-04-05 10:38:45 +0300

    Add files via upload

--- layout.js
+++ layout.js
@@ -1,10 +1,3 @@
-import "./globals.css";
-
-export const metadata = {
-  title: "NOVATECH",
-  description: "Engineering solutions for roads and airfields",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
